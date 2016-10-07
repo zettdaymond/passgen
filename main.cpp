f4f635e5ee9493955222c3fa86c1db42459ba9d3
zettdaymond <zettday@gmail.com>
2016-10-08 03:28:09 +0500

    Add window icon

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <QGuiApplication>
+#include <QIcon>
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
 
@@ -8,6 +9,7 @@ int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QGuiApplication app(argc, argv);
+	app.setWindowIcon( QIcon{":/assets/icon.png"});
 
     QQmlApplicationEngine engine;
 
